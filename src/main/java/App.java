fe83a8cfbc3209ae41edce6d8b0e9757ca953557
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-29 14:46:59 -0700

    created page to add a word

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,5 +16,11 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/words/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/word-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
