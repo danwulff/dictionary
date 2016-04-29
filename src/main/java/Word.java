09ec4f124f9a366777b952751c6bf372f5e15176
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-29 13:46:38 -0700

    unit logic partially complete for Word class, specifically all logic which does not include Definitions

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
 
 public class Word {
+  private String mWord;
+  private static ArrayList<Word> instances = new ArrayList<Word>();
 
   public Word(String word) {
-
+    mWord = word;
+    instances.add(this);
   }
 
   public String getWord() {
-    return "";
+    return mWord;
   }
 
   public static ArrayList<Word> all() {
-    return null;
+    return instances;
   }
 
   public static void clear() {
-
+    instances.clear();
   }
 
 }
