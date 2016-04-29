3bb35e18afeda093160d7f8da70634f2a2bef063
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-29 11:27:56 -0700

    created unit tests for Definition class, created skeleton methods for Definition class. Unit tests failing.

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+
 public class Definition {
 
 
 
   public Definition(String definition) {
+
+  }
+
+  public String getDefinition() {
+    return "";
+  }
+
+  public static ArrayList<Definition> all() {
+    return null;
+  }
+
+  public static void clear() {
 
   }
 
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -11,5 +12,28 @@ public class DefinitionTest {
   public void Definition_instantiatesCorrectly_true() {
     Definition myDefinition = new Definition("To state or set forth meaning");
     assertEquals(true, myDefinition instanceof Definition);
+  }
+
+  @Test
+  public void getDefinition_instantiatesWithDescription_String() {
+    Definition myDefinition = new Definition("To state or set forth meaning");
+    assertEquals("To state or set forth meaning", myDefinition.getDefinition());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfDefinition_True() {
+    Definition.clear();
+    Definition testDefinition1 = new Definition("To state or set forth meaning");
+    Definition testDefinition2 = new Definition("An explanation of the meaning of words");
+    assertTrue(Definition.all().contains(testDefinition1));
+    assertTrue(Definition.all().contains(testDefinition2));
+  }
+
+  @Test
+  public void clear_emptiesAllTasksFromArrayList_0() {
+    Definition testDefinition1 = new Definition("To state or set forth meaning");
+    Definition testDefinition2 = new Definition("An explanation of the meaning of words");
+    Definition.clear();
+    assertEquals(0, (Definition.all()).size());
   }
 }
