1573dbad8b460a44bd0c06d0ec51dfbf363a074a
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-29 11:33:25 -0700

    all Definition unit tests passing.

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,23 +1,25 @@
 import java.util.ArrayList;
 
 public class Definition {
-
+  private String mDefinition;
+  private static ArrayList<Definition> instances = new ArrayList<Definition>();
 
 
   public Definition(String definition) {
-
+    mDefinition = definition;
+    instances.add(this);
   }
 
   public String getDefinition() {
-    return "";
+    return mDefinition;
   }
 
   public static ArrayList<Definition> all() {
-    return null;
+    return instances;
   }
 
   public static void clear() {
-
+    instances.clear();
   }
 
 }
